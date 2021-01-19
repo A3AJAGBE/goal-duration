d3c8ef32c44be397b3d3ec1c53a0bbf0a0fe3488
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-19 05:27:25 +0000

    Add calculation

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 """
 An application that calculates the time constraints in achieving a goal
 """
+
+print('Welcome User, You are using this application because you have a long time goal')
+print('This application will help you calculation the time needed to achieve that goal\n')
+
+current_age = int(input('Your current age: '))
+goal_age = int(input('Age to have achieved that goal: '))
+
+if current_age and goal_age > 0:
+    time_to_achieve = goal_age - current_age
+
+    months = time_to_achieve * 12
+    weeks = time_to_achieve * 52
+    days = time_to_achieve * 365
+
+    print(f"You have {days} days, {weeks} weeks, {months} months and {time_to_achieve} "
+          f"years to achieve your goal. Good-luck 🤞🏾💪🏽.")
+else:
+    print('Invalid inputs.')
+
+
