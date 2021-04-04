44afa36bdc16addebed45a4c875b4c63da565143
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-04 10:45:23 +0100

    Update commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 """
-An application that calculates the time constraints in achieving a goal
+An application that calculates how long a goal will be achieved using age.
 """
-
-print('\nWelcome User, You are using this application because you have a long time goal')
-print('This application will help you calculate the time needed to achieve that goal\n')
+print('\nHow long to achieve that goal?')
 
 
 def duration_app():
