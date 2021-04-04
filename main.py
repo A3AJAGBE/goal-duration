212a9079ea7c6c694ccdea3aecd43c5f7d6c5356
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-04 10:18:33 +0100

    Improved validation

--- main.py
+++ main.py
@@ -2,26 +2,45 @@
 An application that calculates the time constraints in achieving a goal
 """
 
-print('Welcome User, You are using this application because you have a long time goal')
+print('\nWelcome User, You are using this application because you have a long time goal')
 print('This application will help you calculate the time needed to achieve that goal\n')
 
-current_age = int(input('Your current age: '))
-goal_age = int(input('Hope to achieve the goal by what age: '))
 
-if current_age > 1 and (goal_age > current_age):
-    time_to_achieve = goal_age - current_age
+def duration_app():
 
-    if time_to_achieve > 1:
-        months = time_to_achieve * 12
-        weeks = time_to_achieve * 52
-        days = time_to_achieve * 365
+    try:
+        current_age = int(input('Your current age: '))
+    except ValueError:
+        print('Integers Only\n')
+        duration_app()
 
-        print(f"You have {days} days, {weeks} weeks, {months} months and {time_to_achieve} "
-              f"years to achieve your goal. Good-luck 🤞🏾💪🏽.")
+    try:
+        goal_age = int(input('Hope to achieve the goal by what age: '))
+    except ValueError:
+        print('Integers Only\n')
+        duration_app()
     else:
-        print('This application calculates long-term goals.\n'
-              'Although, a one year goal will give you 365 days, 52 weeks, and 12 months to achieve it.')
-else:
-    print('Invalid inputs.')
 
+        if current_age > 5:
+            if goal_age > current_age:
+                time_to_achieve = goal_age - current_age
+
+                if time_to_achieve > 1:
+                    months = time_to_achieve * 12
+                    weeks = time_to_achieve * 52
+                    days = time_to_achieve * 365
+
+                    print(f"\nYou have {days} days, {weeks} weeks, {months} months and {time_to_achieve} "
+                          f"years to achieve your goal. Good-luck 🤞🏾💪🏽.")
+                else:
+                    print('\nThis application calculates long-term goals.\n'
+                          'Although, a one year goal will give you 365 days, 52 weeks, and 12 months to achieve it.')
+            else:
+                print('The age you hope to achieve your goal must be greater than your current age.\n')
+                duration_app()
+        else:
+            print('You must be older than 5 years before you can use this application.')
+
+
+duration_app()
 
